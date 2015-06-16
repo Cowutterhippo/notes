494386c6f064035ddc6b2ff3cbec5929ad33d35a
william <wmantly@gmail.com>
2015-06-16 15:31:41 -0400

    Changed routes to make blog default

--- notes/urls.py
+++ notes/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('users.urls')),
+    url(r'^$', include('blog.urls')),
     url(r'^users/', include('users.urls')),
 	url( r'^blog/', include( 'blog.urls', namespace='blog' ) ),
 )
